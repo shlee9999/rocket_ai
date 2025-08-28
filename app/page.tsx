a50851ce96f94adb09c9a47d219e357171d0ed39
shlee9999 <oklshop555@naver.com>
2025-08-28 23:42:07 +0900

    임시

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,83 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <main className='relative bg-blue-200'>
+      <div className='w-fit m-auto max-w-md flex flex-col'>
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src={'/assets/intro.svg'}
+          alt={'intro'}
+          fill
+          className='!relative'
         />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className='relative w-fit bg-red-500'>
+          <Image
+            src={'/assets/speech_bubble_1.svg'}
+            alt={'speech_bubble_1'}
+            width={215}
+            height={139}
+            // className='object-contain'
+          />
+          <div className='absolute inset-0 flex flex-col items-center justify-center text-center pt-[23px]'>
+            <p className='text-black font-medium text-sm whitespace-nowrap'>
+              이제 본격적으로
+            </p>
+            <p className='text-black font-medium text-sm whitespace-nowrap'>
+              OO님의 사주팔자를
+            </p>
+            <p className='text-black font-medium text-sm whitespace-nowrap'>
+              분석해볼 차례네요.
+            </p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <div className='relative  bg-red-500'>
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src={'/assets/deco_2.svg'}
+            alt={'deco_2'}
+            width={153}
+            height={120}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+            className='self-end mr-[24px]'
+            src={'/assets/writing.svg'}
+            alt={'writing'}
+            width={120}
+            height={285}
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        </div>
+
+        <div className='relative w-fit bg-red-500'>
           <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+            src={'/assets/speech_bubble_2.svg'}
+            alt={'speech_bubble_2'}
+            width={239}
+            height={138.78}
           />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <div className='absolute inset-0 flex flex-col items-center justify-center text-center'>
+            <p className='text-black font-medium text-sm whitespace-nowrap'>
+              제가 oo님의 사주를
+            </p>
+            <p className='text-black font-medium text-sm whitespace-nowrap'>
+              보기 쉽게 표로 정리했어요
+            </p>
+          </div>
+        </div>
+        <Image
+          src={'/assets/talking.svg'}
+          alt={'talking'}
+          fill
+          className='!relative'
+        />
+        <div>
+          <Image src={'/assets/left.svg'} alt={'left'} fill />
+          <Image
+            src={'/assets/right.svg'}
+            alt={'right'}
+            width={100}
+            height={100}
+          />
+        </div>
+      </div>
+    </main>
   );
 }
