74dd53d7aa102d3ee8c04e4afcd7341d3280fcb0
shlee9999 <oklshop555@naver.com>
2025-09-01 08:02:16 +0900

    feat: globals.css에 폰트 크기 토큰 추가 및 SajuTable, SajuTableCell, SajuTableSection 컴포넌트에서 TailwindCSS 클래스 적용

--- components/SajuTable.tsx
+++ components/SajuTable.tsx
@@ -1,7 +1,7 @@
-import { SajuTableCell } from '@/components/SajuTableCell';
-import { Row } from '@/types/SajuTable';
-import { cn } from '@/utils/cn';
-import { pxu } from '@/utils/pxu';
+import { SajuTableCell } from "@/components/SajuTableCell";
+import { Row } from "@/types/SajuTable";
+import { cn } from "@/utils/cn";
+import { pxu } from "@/utils/pxu";
 
 interface SajuTableProps {
   columns: string[];
@@ -11,23 +11,23 @@ interface SajuTableProps {
 export default function SajuTable({ columns, data }: SajuTableProps) {
   return (
     <table
-      className='w-full border-collapse focus-visible:outline-2 focus-visible:outline-blue-500'
+      className="w-full border-collapse focus-visible:outline-2 focus-visible:outline-blue-500"
       tabIndex={0}
       style={{
         marginTop: pxu(20),
         marginBottom: pxu(32),
       }}
     >
-      <caption className='sr-only'>사주 정보 테이블</caption>
+      <caption className="sr-only">사주 정보 테이블</caption>
       <thead>
         <tr>
-          <th className='border-r-2 border-r-black' />
+          <th className="border-r-2 border-r-black" />
           {columns.map((c, colIndex) => (
             <th
               key={c}
-              scope='col'
+              scope="col"
               id={`col-${colIndex}`}
-              className='border-r-1 border-r-border-light [&:last-child]:border-r-2 [&:last-child]:border-r-black font-size-lg'
+              className="border-r-1 border-r-border-light [&:last-child]:border-r-2 [&:last-child]:border-r-black text-lg"
               style={{
                 lineHeight: 2,
               }}
@@ -42,24 +42,24 @@ export default function SajuTable({ columns, data }: SajuTableProps) {
           <tr
             key={rowIndex}
             className={cn(
-              'border-t-2 border-t-black',
-              rowIndex === 2 && 'border-t border-t-border-light',
-              '[&:last-child]:border-b-2 [&:last-child]:border-b-black'
+              "border-t-2 border-t-black",
+              rowIndex === 2 && "border-t border-t-border-light",
+              "[&:last-child]:border-b-2 [&:last-child]:border-b-black"
             )}
           >
             <th
-              scope='row'
+              scope="row"
               id={`row-${rowIndex}`}
-              className='border-r-2 border-r-black'
+              className="border-r-2 border-r-black"
             >
               <SajuTableCell
                 cell={{
                   classNameChinese: cn(
-                    'font-size-2xs',
+                    "text-2xs",
                     row.rowHeader.classNameChinese
                   ),
                   classNameKorean: cn(
-                    'font-size-4xs',
+                    "text-4xs",
                     row.rowHeader.classNameKorean
                   ),
                   ...row.rowHeader,
@@ -70,7 +70,7 @@ export default function SajuTable({ columns, data }: SajuTableProps) {
               <td
                 key={cellIndex}
                 headers={`row-${rowIndex} col-${cellIndex}`}
-                className='bg-tertiary-bg p-[5px] border border-border-light [&:last-child]:border-r-2 [&:last-child]:border-r-black'
+                className="bg-tertiary-bg p-[5px] border border-border-light [&:last-child]:border-r-2 [&:last-child]:border-r-black"
               >
                 <SajuTableCell cell={cell} />
               </td>
--- components/SajuTableSection.tsx
+++ components/SajuTableSection.tsx
@@ -1,19 +1,19 @@
-import SajuTable from '@/components/SajuTable';
-import { figmaLayout } from '@/constants/layout';
-import { sajuColumns, sajuData } from '@/data/saju-data';
-import ImageWithWrapper from './ImageWithWrapper';
-import { pxu } from '@/utils/pxu';
+import SajuTable from "@/components/SajuTable";
+import { figmaLayout } from "@/constants/layout";
+import { sajuColumns, sajuData } from "@/data/saju-data";
+import ImageWithWrapper from "./ImageWithWrapper";
+import { pxu } from "@/utils/pxu";
 
 export default function SajuTableSection() {
   return (
     <div
-      className=''
+      className=""
       style={{
         paddingInline: pxu(figmaLayout.sajuTablePaddingInline),
       }}
     >
       <div
-        className='relative bg-secondary-bg w-full h-full border-border-primary'
+        className="relative bg-secondary-bg w-full h-full border-border-primary"
         style={{
           paddingBlock: pxu(figmaLayout.sajuTableOuterPadding),
           borderWidth: figmaLayout.sajuTableOuterBorderWidth,
@@ -21,7 +21,7 @@ export default function SajuTableSection() {
       >
         {/* 가로 border를 가진 영역 */}
         <div
-          className='border-y-[1px] border-border-secondary'
+          className="border-y-[1px] border-border-secondary"
           style={{
             top: pxu(8),
             bottom: pxu(8),
@@ -30,16 +30,16 @@ export default function SajuTableSection() {
         >
           {/* 내부 Width: figmaLayout.sajuTableWidth - 24 */}
           <div
-            className='flex flex-col justify-between items-center'
+            className="flex flex-col justify-between items-center"
             style={{
               marginTop: pxu(32),
             }}
           >
-            <span className='font-size-sm' style={{ lineHeight: 1 }}>
+            <span className="text-sm" style={{ lineHeight: 1 }}>
               김로켓님의 사주
             </span>
             <span
-              className='font-bold font-size-lg'
+              className="font-bold text-lg"
               style={{
                 lineHeight: 1,
                 marginTop: pxu(12),
@@ -59,24 +59,24 @@ export default function SajuTableSection() {
         </div>
         {/* Decorations */}
         <ImageWithWrapper
-          src='/assets/left.svg'
-          alt='left'
+          src="/assets/left.svg"
+          alt="left"
           ratioWidth={figmaLayout.sajuTableDecoWidth}
           ratioHeight={figmaLayout.sajuTableDecoHeight}
           style={{
-            position: 'absolute',
+            position: "absolute",
             width: pxu(figmaLayout.sajuTableDecoWidth),
             top: pxu(figmaLayout.sajuTableDecoLeftTop),
             left: pxu(figmaLayout.sajuTableDecoLeftLeft),
           }}
         />
         <ImageWithWrapper
-          src='/assets/right.svg'
-          alt='right'
+          src="/assets/right.svg"
+          alt="right"
           ratioWidth={figmaLayout.sajuTableDecoWidth}
           ratioHeight={figmaLayout.sajuTableDecoHeight}
           style={{
-            position: 'absolute',
+            position: "absolute",
             width: pxu(figmaLayout.sajuTableDecoWidth),
             top: pxu(figmaLayout.sajuTableDecoRightTop),
             right: pxu(figmaLayout.sajuTableDecoRightRight),
@@ -85,7 +85,7 @@ export default function SajuTableSection() {
 
         {/* 세로 border 박스 */}
         <div
-          className='absolute border-x-[1px] border-border-secondary'
+          className="absolute border-x-[1px] border-border-secondary"
           style={{
             top: 0,
             bottom: 0,
